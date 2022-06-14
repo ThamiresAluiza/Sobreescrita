0be262fed16ca95ca660314a6bea6a7cc224d211
ThamiresAluiza <thamiresluizasantos@hotmail.com>
2022-06-14 16:04:43 -0300

    projeto override

---
+++ src/main.java
@@ -0,0 +1,24 @@
+//Override , mesma assinatura em classes diferentes
+public class main {
+    public static void (String[] args) {
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+        Canguru c = new Canguru();
+        Cachorro k = new Cachorro();
+        Cobra j = new Cobra();
+        Tartaruga t = new Tartaruga();
+        Arara e = new Arara();
+        GoldFish g = new GoldFish();
+
+        c.locomover();
+        k.locomover();
+        k.emitirSom();
+
+
+
+
+    }
+
+}
